9eb719d07201286135c2e5c344f74da21048cbd3
anandwetzel <anandwetzel@gmail.com>
2025-04-02 21:27:40 -0400

    P2

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Anand Wetzel on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
